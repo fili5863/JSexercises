3c1acaf5dfea8c81f500a0e0522d292f4d72e91d
Jachob Westh <jachobwesth@gmail.com>
2023-02-14 14:39:51 +0100

    exercise2

--- js9/functionAndReturn/experiment.js
+++ js9/functionAndReturn/experiment.js
@@ -1,3 +1,5 @@
+let hexValue = "#87ea34";
+
 function hexToRGB(hexColor) {
   const r = parseInt(hexColor.substring(1, 3), 16);
   const g = parseInt(hexColor.substring(3, 5), 16);
@@ -10,3 +12,11 @@ function hexToRGB(hexColor) {
 console.log("Color as RGB", hexToRGB("#f80ea3"));
 
 console.log("Red component:", hexToRGB("#f80ea3").r);
+
+function createPerson(firstName, lastName, age, isGinger) {
+  return { firstName, lastName, age, isGinger };
+}
+
+let newPerson = createPerson("Peter", "Tolstrup", 44, true);
+
+console.log(newPerson.firstName);
